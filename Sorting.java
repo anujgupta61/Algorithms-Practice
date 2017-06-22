5da7d519ae46dcb690b0c5280972d4a9c99fe4cc
Anuj Gupta <anujgupta2004@gmail.com>
2017-06-23 00:06:20 +0530

    Adding Merge and Shell sort

--- Sorting.java
+++ Sorting.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 
 class MySorting {
-	private static final int MAXSIZE = 50;
-	private int[] arr = new int[MAXSIZE];
-	public int n;
+	private int[] arr ;
+	private int order = 1;
+	public int n; // 1: Ascending, 2: Descending
 
 	public void setArray() {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("No. of elements you want to insert: ");
 		this.n = scan.nextInt();
+		this.arr = new int[this.n];
 		System.out.println("Enter array elements -");
 		for(int i = 0; i < this.n; i ++)
 			this.arr[i] = scan.nextInt();
 		System.out.println("Array has been inserted ...");
 		this.displayArray();
+	}
+
+	public void setOrder() {
+		Scanner scan = new Scanner(System.in);
+		int temp = 0;
+		while(true) {
+			System.out.print("1: Ascending --- OR --- 2: Descending ? - ");
+			temp = scan.nextInt();
+			if(temp == 1 || temp == 2)
+				break;
+			else
+				System.out.println("Invalid choice. Try again.");		
+		}
+		this.order = temp;
 	}
 
 	public void displayArray() {
@@ -23,12 +38,12 @@ class MySorting {
 		System.out.print("\n");
 	}
 
-	public void bubbleSort(int order) { // 1: Ascending, 2: Descending
+	public void bubbleSort() { 
 		int[] a = this.arr;
-		for(int i = n - 1; i > 0; i --) {
+		for(int i = this.n - 1; i > 0; i --) {
 			boolean anyswap = false;
 			for(int j = 0; j < i; j ++) {
-				boolean swap = (order == 1) ? (a[j] > a[j + 1]) : (a[j] < a[j + 1]);
+				boolean swap = (this.order == 1) ? (a[j] > a[j + 1]) : (a[j] < a[j + 1]);
 				if(swap) {
 					anyswap = true;
 					int temp = a[j];
@@ -44,29 +59,29 @@ class MySorting {
 		this.displayArray();
 	}
 
-	public void insertionSort(int order) { // 1: Ascending, 2: Descending
+	public void insertionSort(boolean doPrint) { // 1: Ascending, 2: Descending
 		int[] a = this.arr;
-		for(int i = 0; i < n - 1; i ++) {
+		for(int i = 0; i < this.n - 1; i ++) {
 			int j = i, num = a[i + 1];
-			while(((order == 1) ? (num < a[j]) : (num > a[j])) && (j >= 0)) {
+			while((j >= 0) && ((this.order == 1) ? (num < a[j]) : (num > a[j]))) {
 				a[j + 1] = a[j];
-				if(j == 0)
-					break;
 				j --;
 			}
-			a[j] = num;
+			a[j + 1] = num;
 		}
 		this.arr = a;
-		System.out.println("Array sorted ...");
-		this.displayArray();
+		if(doPrint) {
+			System.out.println("Array sorted ...");
+			this.displayArray();
+		}
 	}
 
-	public void selectionSort(int order) { // 1: Ascending, 2: Descending
+	public void selectionSort() { // 1: Ascending, 2: Descending
 		int[] a = this.arr;
 		for(int i = 0; i < n - 1; i ++) {
 			int mj = i;
 			for(int j = i + 1; j < n ; j ++) {
-				if((order == 1) ? (a[mj] > a[j]) : (a[mj] < a[j]))
+				if((this.order == 1) ? (a[mj] > a[j]) : (a[mj] < a[j]))
 					mj = j;
 			}
 			if(mj != i) {
@@ -79,6 +94,61 @@ class MySorting {
 		System.out.println("Array sorted ...");
 		this.displayArray();
 	}
+
+	private void merge(int first, int mid, int last) {
+		int[] a = this.arr;
+		int[] temp = new int[last - first + 1];
+		int i = 0, j = first, k = mid + 1;
+		while(j <= mid && k <= last) {
+			if((this.order == 1) ? (a[j] <= a[k]) : (a[j] >= a[k]))
+				temp[i ++] = a[j ++];
+			else
+				temp[i ++] = a[k ++];
+		}
+		while(j <= mid)
+			temp[i ++] = a[j ++];
+		while(k <= last)
+			temp[i ++] = a[k ++];
+		for(int l = 0; l < i; l ++)
+			a[first + l] = temp[l];
+		this.arr = a;
+	}
+
+	public void mergeSort(int first, int last) {
+		if(first == last)
+			return;
+		int mid = (first + last) / 2;
+		mergeSort(first, mid);
+		mergeSort(mid + 1, last);
+		this.merge(first, mid, last);
+	}
+
+	public void shellSort() { // Uses Insertion sort, Improvement over Insertion sort
+		int[] a = this.arr; // Copy here using loop, not assign
+		int k = 1, m = this.n;
+		while(3 * k + 1 <= m)
+			k = 3 * k + 1;
+		while(k > 1) {
+			for(int i = 0; (i + k) < m; i ++) {
+				int size = (int)(Math.floor((m - i - 1) / k)) + 1;
+				int[] temp = new int[size];
+				for(int j = i, l = 0; j < m; j += k, l ++)
+					temp[l] = a[j];
+				this.arr = temp;
+				this.n = size;
+				this.insertionSort(false);
+				temp = this.arr;
+				for(int j = i, l = 0; j < m; j += k, l ++)
+					a[j] = temp[l];
+			}
+			k = (k - 1) / 3;
+		}
+		this.arr = a;
+		this.n = m;
+		this.insertionSort(false);
+		System.out.println("Array sorted ...");
+		this.displayArray();
+	}
 }
 
 public class Sorting {
@@ -87,7 +157,8 @@ public class Sorting {
 		Scanner scan = new Scanner(System.in);
 		int choice = 0;
 		while(choice != 6) {
-			System.out.println("1: Insert new array\n2: Bubble sort\n3: Insertion sort\n4: Selection sort\n5: Display array\n6: Exit");
+			System.out.println("1: Insert new array\n2: Bubble sort\n3: Insertion sort\n4: Selection sort\n5: Display array\n6: Exit\n" + 
+				"7: Merge sort\n8: Shell sort");
 			System.out.print("Choice: ");
 			choice = scan.nextInt();
 			switch(choice) {
@@ -99,9 +170,8 @@ public class Sorting {
 					if(ms.n == 0)
 						System.out.println("Array is empty. Insert new one.");
 					else {	
-						System.out.print("1: Ascending --- OR --- 2: Descending ? - ");
-						int order = scan.nextInt();
-						ms.bubbleSort(order);
+						ms.setOrder();
+						ms.bubbleSort();
 					}
 					break;
 				}
@@ -109,9 +179,8 @@ public class Sorting {
 					if(ms.n == 0)
 						System.out.println("Array is empty. Insert new one.");
 					else {
-						System.out.print("1: Ascending --- OR --- 2: Descending ? - ");
-						int order = scan.nextInt();
-						ms.insertionSort(order);
+						ms.setOrder();
+						ms.insertionSort(true);
 					}
 					break;
 				}
@@ -119,9 +188,8 @@ public class Sorting {
 					if(ms.n == 0)
 						System.out.println("Array is empty. Insert new one.");
 					else {
-						System.out.print("1: Ascending --- OR --- 2: Descending ? - ");
-						int order = scan.nextInt();
-						ms.selectionSort(order);
+						ms.setOrder();
+						ms.selectionSort();
 					}
 					break;
 				}
@@ -130,6 +198,26 @@ public class Sorting {
 					break;
 				}
 				case 6: {
+					break;
+				}
+				case 7: {
+					if(ms.n == 0)
+						System.out.println("Array is empty. Insert new one.");
+					else {
+						ms.setOrder();
+						ms.mergeSort(0, ms.n - 1);
+						System.out.println("Array sorted ...");
+						ms.displayArray();
+					}
+					break;
+				}
+				case 8: {
+					if(ms.n == 0)
+						System.out.println("Array is empty. Insert new one.");
+					else {
+						ms.setOrder();
+						ms.shellSort();
+					}
 					break;
 				}
 				default: {
